34939d096bac08c366a0fb1851d9ceff6f3fe063
清梦 <90257473+cyljx9999@users.noreply.github.com>
2024-06-21 00:31:57 +0800

    聊天页面开发-3-(新增账号隐私设置)

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -7,12 +7,22 @@ export const getPersonalApi = ()=>{
     return request.get(`/user/getPersonalInfo`)
 }
 
-// 获取修改个人信息
+// 修改个人信息
 export const alterPersonalInfoApi = (params)=>{
     return request.put(`/user/alterPersonalInfo`,params)
 }
 
-// 获取修改个人账号
+// 修改个人账号
 export const alterAccountApi = (params)=>{
     return request.put(`/user/alterAccount`,params)
+}
+
+// 获取个人隐私设置信息信息
+export const getPrivacySettingInfoApi = ()=>{
+    return request.get(`/user/getPersonalPrivacySetting`)
+}
+
+// 修改个人隐私设置信息信息
+export const alterPrivacySettingInfoApi = (params)=>{
+    return request.put(`/user/alterPersonalPrivacySetting`,params)
 }
--- src/views/chat/cpns/head/ChatHead.vue
+++ src/views/chat/cpns/head/ChatHead.vue
@@ -33,13 +33,14 @@
             <template #dropdown>
               <el-dropdown-menu>
                 <el-dropdown-item command="personalInfo">个人信息</el-dropdown-item>
+                <el-dropdown-item command="setting">账号设置</el-dropdown-item>
                 <el-dropdown-item command="logout">退出登陆</el-dropdown-item>
               </el-dropdown-menu>
             </template>
           </el-dropdown>
         </div>
       </div>
-      <el-drawer v-model="pageData.drawerFlag" direction="rtl">
+      <el-drawer v-model="pageData.personalInfoDrawerFlag" direction="rtl">
         <template #header>
           <h4>个人信息</h4>
         </template>
@@ -47,7 +48,7 @@
           <el-form
               label-position="right"
               label-width="auto"
-              :model="pageData.formData"
+              :model="pageData.personalInfoFormData"
               style="max-width: 600px"
               :rules="personalInfoRules"
               ref="personalInfoRef"
@@ -61,7 +62,7 @@
               <el-col :span="6">
                 <el-button
                     class="btn btn-alter"
-                    :disabled="pageData.formData.alterAccountCount === 0"
+                    :disabled="pageData.personalInfoFormData.alterAccountCount === 0"
                     @click="showAlterAccountDialog"
                 >修改
                 </el-button>
@@ -69,13 +70,13 @@
             </el-row>
 
             <el-form-item label="用户名" prop="userName">
-              <el-input v-model="pageData.formData.userName"/>
+              <el-input v-model="pageData.personalInfoFormData.userName"/>
             </el-form-item>
             <el-form-item label="手机号" prop="userPhone">
-              <el-input v-model="pageData.formData.userPhone"/>
+              <el-input v-model="pageData.personalInfoFormData.userPhone"/>
             </el-form-item>
             <el-form-item label="性别" prop="userSex">
-              <el-radio-group v-model="pageData.formData.userSex" class="ml-4">
+              <el-radio-group v-model="pageData.personalInfoFormData.userSex" class="ml-4">
                 <el-radio :value="0" size="large">女</el-radio>
                 <el-radio :value="1" size="large">男</el-radio>
                 <el-radio :value="2" size="large">隐藏</el-radio>
@@ -86,17 +87,172 @@
                                       @upload-image-success="uploadImageSuccess"></custom-img-upload-file>
             </el-form-item>
             <el-form-item label="我的二维码">
-              <el-image class="img" :src="pageData.formData.qrcodeUrl"></el-image>
+              <el-image class="img" :src="pageData.personalInfoFormData.qrcodeUrl"></el-image>
             </el-form-item>
           </el-form>
         </template>
         <template #footer>
           <div style="flex: auto">
-            <el-button @click="cancelDrawerClick">关闭</el-button>
-            <el-button class="btn" type="primary" @click="confirmDrawerClick">提交修改</el-button>
+            <el-button @click="cancelPersonalInfoDrawerClick">关闭</el-button>
+            <el-button class="btn" type="primary" @click="confirmPersonalInfoDrawerClick">提交修改</el-button>
           </div>
         </template>
       </el-drawer>
+
+      <el-drawer v-model="pageData.privacySettingDrawerFlag" direction="rtl">
+        <template #header>
+          <h4>账号信息设置</h4>
+        </template>
+        <template #default>
+          <el-form
+              label-position="right"
+              label-width="auto"
+              :model="pageData.privacySettingFormData"
+              style="max-width: 600px"
+              :rules="personalInfoRules"
+              ref="personalInfoRef"
+              class="setting-form"
+          >
+            <el-divider content-position="center">可以通过以下方式找到我</el-divider>
+            <el-form-item label="账号">
+              <el-switch
+                  v-model="pageData.privacySettingFormData.findByAccount"
+                  class="mt-2"
+                  :active-value="1"
+                  :inactive-value="0"
+                  style="margin-left: 24px"
+                  inline-prompt
+                  size="large"
+                  width="100px"
+                  :active-icon="Check"
+                  :inactive-icon="Close"
+                  @change="privacySettingChange"
+              />
+            </el-form-item>
+            <el-form-item label="手机号">
+              <el-switch
+                  v-model="pageData.privacySettingFormData.findByPhone"
+                  class="mt-2"
+                  :active-value="1"
+                  :inactive-value="0"
+                  style="margin-left: 24px"
+                  inline-prompt
+                  size="large"
+                  width="100px"
+                  :active-icon="Check"
+                  :inactive-icon="Close"
+                  @change="privacySettingChange"
+              />
+            </el-form-item>
+
+            <el-divider content-position="center">可以通过以下方式添加我</el-divider>
+            <el-form-item label="账号">
+              <el-switch
+                  v-model="pageData.privacySettingFormData.addByAccount"
+                  class="mt-2"
+                  :active-value="1"
+                  :inactive-value="0"
+                  style="margin-left: 24px"
+                  inline-prompt
+                  size="large"
+                  width="100px"
+                  :active-icon="Check"
+                  :inactive-icon="Close"
+                  @change="privacySettingChange"
+              />
+            </el-form-item>
+            <el-form-item label="手机号">
+              <el-switch
+                  v-model="pageData.privacySettingFormData.addByPhone"
+                  class="mt-2"
+                  :active-value="1"
+                  :inactive-value="0"
+                  style="margin-left: 24px"
+                  inline-prompt
+                  size="large"
+                  width="100px"
+                  :active-icon="Check"
+                  :inactive-icon="Close"
+                  @change="privacySettingChange"
+              />
+            </el-form-item>
+            <el-form-item label="群聊">
+              <el-switch
+                  v-model="pageData.privacySettingFormData.addByGroup"
+                  class="mt-2"
+                  :active-value="1"
+                  :inactive-value="0"
+                  style="margin-left: 24px"
+                  inline-prompt
+                  size="large"
+                  width="100px"
+                  :active-icon="Check"
+                  :inactive-icon="Close"
+                  @change="privacySettingChange"
+              />
+            </el-form-item>
+            <el-form-item label="名片">
+              <el-switch
+                  v-model="pageData.privacySettingFormData.addByCard"
+                  class="mt-2"
+                  :active-value="1"
+                  :inactive-value="0"
+                  style="margin-left: 24px"
+                  inline-prompt
+                  size="large"
+                  width="100px"
+                  :active-icon="Check"
+                  :inactive-icon="Close"
+                  @change="privacySettingChange"
+              />
+            </el-form-item>
+            <el-form-item label="二维码">
+              <el-switch
+                  v-model="pageData.privacySettingFormData.addByQrcode"
+                  class="mt-2"
+                  :active-value="1"
+                  :inactive-value="0"
+                  style="margin-left: 24px"
+                  inline-prompt
+                  size="large"
+                  width="100px"
+                  :active-icon="Check"
+                  :inactive-icon="Close"
+                  @change="privacySettingChange"
+              />
+            </el-form-item>
+            <el-divider></el-divider>
+            <el-tooltip
+                class="box-item"
+                effect="dark"
+                content="设置后，朋友拍你的时候将会出现"
+                placement="top-start"
+            >
+              <el-form-item label="拍一拍">
+                <el-row :gutter="24">
+                  <el-col :span="6">
+                    <span class="default-text">朋友拍了拍我</span>
+                  </el-col>
+                  <el-col :span="18">
+                    <el-input type="text" maxlength="10" v-model="pageData.privacySettingFormData.patContent"/>
+                  </el-col>
+                </el-row>
+              </el-form-item>
+            </el-tooltip>
+            <el-form-item label="个性签名">
+              <el-input type="textarea" maxlength="20" v-model="pageData.privacySettingFormData.personalizedSignature"/>
+            </el-form-item>
+
+          </el-form>
+        </template>
+        <template #footer>
+          <div style="flex: auto">
+            <el-button @click="cancelPrivacySettingDrawerClick">关闭</el-button>
+            <el-button class="btn" type="primary" @click="confirmPrivacySettingDrawerClick">提交修改</el-button>
+          </div>
+        </template>
+      </el-drawer>
+
       <custom-dialog
           :title="alterAccountDialogConfig.title"
           :visible="alterAccountDialogConfig.visible"
@@ -130,8 +286,14 @@
 import {getCurrentInstance, onUpdated, reactive, ref} from "vue";
 import CustomImgUploadFile from "@/components/fileUpload/CustomImgUploadFile.vue";
 import {rsaEncrypt} from "@/utils/JSEncryptUtil.ts";
-import {alterAccountApi, alterPersonalInfoApi} from "@/api/userApi.ts";
+import {
+  alterAccountApi,
+  alterPersonalInfoApi,
+  alterPrivacySettingInfoApi,
+  getPrivacySettingInfoApi
+} from "@/api/userApi.ts";
 import CustomDialog from "@/components/dialog/CustomDialog.vue";
+import {Check, Close} from "@element-plus/icons-vue";
 
 const {proxy} = getCurrentInstance()
 
@@ -144,16 +306,16 @@ const props = defineProps({
 
 
 onUpdated(() => {
-  initFormData()
+  initPersonalInfoFormData()
 })
 // 初始化表单数据
-const initFormData = () => {
-  pageData.formData.userName = props.personInfo.userName
-  pageData.formData.userPhone = props.personInfo.userPhone
-  pageData.formData.userSex = props.personInfo.userSex
-  pageData.formData.userAccount = props.personInfo.userAccount
-  pageData.formData.qrcodeUrl = props.personInfo.qrcodeUrl
-  pageData.formData.alterAccountCount= props.personInfo.alterAccountCount
+const initPersonalInfoFormData = () => {
+  pageData.personalInfoFormData.userName = props.personInfo.userName
+  pageData.personalInfoFormData.userPhone = props.personInfo.userPhone
+  pageData.personalInfoFormData.userSex = props.personInfo.userSex
+  pageData.personalInfoFormData.userAccount = props.personInfo.userAccount
+  pageData.personalInfoFormData.qrcodeUrl = props.personInfo.qrcodeUrl
+  pageData.personalInfoFormData.alterAccountCount= props.personInfo.alterAccountCount
   if (pageData.fileList.length === 0) {
     pageData.fileList.push({url: props.personInfo.userAvatar})
   }
@@ -162,9 +324,10 @@ const initFormData = () => {
 const personalInfoRef = ref();
 const alterAccountRef = ref();
 const pageData = reactive({
-  drawerFlag: false,
+  personalInfoDrawerFlag: false,
+  privacySettingDrawerFlag: false,
   fileList: [],
-  formData: {
+  personalInfoFormData: {
     userName: "",
     userAccount: "",
     userPhone: "",
@@ -175,6 +338,17 @@ const pageData = reactive({
   },
   alterAccountForm: {
     userAccount: ""
+  },
+  privacySettingFormData:{
+    addByAccount: 0,
+    addByPhone: 0,
+    addByGroup: 0,
+    addByCard: 0,
+    addByQrcode: 0,
+    findByAccount: 0,
+    findByPhone: 0,
+    personalizedSignature: "",
+    patContent: ""
   }
 })
 // 修改账号弹出框设置
@@ -214,34 +388,37 @@ const personalInfoRules = {
 
 // 上传图片成功回调
 const uploadImageSuccess = (fileUrl) => {
-  pageData.formData.userAvatar = fileUrl
+  pageData.personalInfoFormData.userAvatar = fileUrl
 }
 
 // 下拉菜单点击事件
 const handleCommand = (command) => {
   if (command === 'personalInfo') {
-    initFormData()
-    pageData.drawerFlag = true
+    initPersonalInfoFormData()
+    pageData.personalInfoDrawerFlag = true
+  } else if (command === "setting") {
+    initPrivacySettingFormData()
+    pageData.privacySettingDrawerFlag = true
   } else if (command === 'logout') {
 
   }
 }
 const emit = defineEmits(['updatePersonalInfo'])
-// 抽屉关闭事件
-const cancelDrawerClick = () => {
-  pageData.drawerFlag = false
+// 个人信息抽屉关闭事件
+const cancelPersonalInfoDrawerClick = () => {
+  pageData.personalInfoDrawerFlag = false
   personalInfoRef.value.resetFields()
 }
-// 抽屉提交事件
-const confirmDrawerClick = () => {
+// 个人信息抽屉提交事件
+const confirmPersonalInfoDrawerClick = () => {
   personalInfoRef.value.validate((valid) => {
     if (valid) {
       const sendData = {
         encryptStr: ""
       }
-      sendData.encryptStr = rsaEncrypt(pageData.formData)
+      sendData.encryptStr = rsaEncrypt(pageData.personalInfoFormData)
       alterPersonalInfoApi(sendData).then(res => {
-        cancelDrawerClick()
+        cancelPersonalInfoDrawerClick()
         proxy.$message.success(res.msg)
         emit('updatePersonalInfo')
       })
@@ -258,19 +435,47 @@ const closeAlterAccountDialog = () => {
   alterAccountDialogConfig.loading = false;
   alterAccountDialogConfig.visible = false
 }
+// 账号弹出框提交
 const confirmAlterAccountDialog = () => {
   alterAccountRef.value.validate((valid) => {
     if (valid) {
       alterAccountDialogConfig.loading = true;
       alterAccountApi(pageData.alterAccountForm).then(res => {
         closeAlterAccountDialog()
-        cancelDrawerClick()
+        cancelPersonalInfoDrawerClick()
         proxy.$message.success(res.msg)
         emit('updatePersonalInfo')
       })
     }
   })
 }
+
+// 获取隐私设置信息
+const initPrivacySettingFormData=()=>{
+  getPrivacySettingInfoApi().then(res =>{
+    pageData.privacySettingFormData = res.data
+  })
+}
+// 隐私设置改变事件
+const privacySettingChange= ()=>{
+  alterPrivacySettingInfoApi(pageData.privacySettingFormData).then(res => {
+    proxy.$message.success(res.msg)
+    initPrivacySettingFormData()
+  })
+}
+
+// 隐私设置抽屉关闭事件
+const cancelPrivacySettingDrawerClick = () => {
+  pageData.privacySettingDrawerFlag = false
+}
+// 隐私设置抽屉提交事件
+const confirmPrivacySettingDrawerClick = () => {
+  alterPrivacySettingInfoApi(pageData.privacySettingFormData).then(res => {
+    proxy.$message.success(res.msg)
+    initPrivacySettingFormData()
+  })
+}
+
 </script>
 
 
@@ -366,6 +571,16 @@ const confirmAlterAccountDialog = () => {
     border-radius: 50%;
   }
 
+  .setting-form{
+    .default-text{
+      color: #b7b1b1;
+    }
+    :deep(.el-row){
+      width: 100%;
+    }
+  }
+
+
   :deep(.el-radio.el-radio--large) {
     height: 32px;
   }
@@ -389,6 +604,9 @@ const confirmAlterAccountDialog = () => {
     color: $system-color;
     border-color: $system-color;
     background-color: white;
+  }
+  :deep(.el-form-item__content) {
+    justify-content: flex-end;
   }
 }
 </style>
